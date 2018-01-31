b3affc60c5aa4227e40da01fdcbc40885c9fca95
Luis Daniel Assulfi <lassulfi@gmail.com>
2018-01-31 13:20:07 -0200

    Atualização do exercício

--- GuiServiceServer/ServiceServerImpl.java
+++ GuiServiceServer/ServiceServerImpl.java
@@ -15,6 +15,26 @@ public class ServiceServerImpl extends UnicastRemoteObject implements ServiceSer
 		serviceList = new HashMap();
 		serviceList.put("Dice Rolling Service", new DiceService());
 		serviceList.put("Day of the Week Service", new DayOfTheWeekService());
+		serviceList.put("Visual Music Service", new MiniMusicService());
 		
+	}
+	
+	public Object[] getServiceList(){
+		System.out.println("in remote");
+		return serviceList.keySet().toArray();
+	}
+	
+	public Service getService(Object serviceKey) throws RemoteException{
+		Service theService = (Service) serviceList.get(serviceKey);
+		return theService;
+	}
+	
+	public static void main(String[] args){
+		try{
+			Naming.rebind("ServiceServer", new ServiceServerImpl());
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}
+		System.out.println("Remote service is running...");
 	}
 }
